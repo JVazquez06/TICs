33c265b650376d06e3057ca1c6f9ea97e10aacce
JVazquez06 <jvazquez82@ucol.mx>
2026-02-12 16:16:44 -0600

    Agenda fixed

--- JS/agenda.js
+++ JS/agenda.js
@@ -1,23 +1,22 @@
-document.write('Una agenda');
-let option = parseInt(prompt('Escoge una opción: \n 1-Agregar \n 2-Listar \n 3-Salir'));
-document.write(option);
 let seguir = true;
-let data = [];
+let datos = [];
 
 while(seguir) {
 let option = parseInt(prompt('Escoge una opción: \n 1-Agregar \n 2-Listar \n 3-Salir'));
 switch(option){
     case 1:
         alert('Escogiste la opción 1');
-        let = nombre = prompt('Ingresa tu nombre');
-        data.push(nombre);
-        console.table(data);
+        let nombre = prompt('Ingresa tu nombre');
+        datos.push(nombre);
         break;
     case 2:
         alert('Escogiste la opción 2');
+        document.writeln(datos);
+        console.table(datos);
         break;
     case 3:
         alert('Escogiste 3 salir');
+        seguir = false;
         break;
     default:
         alert('No recibí una opción válida');
