de8a32bebf527498276108320724f6828a205ed4
JVazquez06 <jvazquez82@ucol.mx>
2026-02-13 08:09:14 -0600

    Agenda con todos los datos

--- JS/agenda.js
+++ JS/agenda.js
@@ -7,16 +7,36 @@ switch(option){
     case 1:
         alert('Escogiste la opción 1');
         let nombre = prompt('Ingresa tu nombre');
-        datos.push(nombre);
+        let apellido = prompt('Ingresa tu apellido');
+        let facultad = prompt ('Ingresa la facultad a la que asistes');
+        let edad = prompt('Ingresa tu edad');
+        let telefono = ('Ingresa tu teléfono');
+        let agenda = {nombre, apellido, facultad, edad, telefono}
+        datos.push(agenda);
+        alert('Contacto creado');
         break;
     case 2:
-        alert('Escogiste la opción 2');
-        document.writeln(datos);
-        console.table(datos);
-        break;
+        let todos = '';
+
+            for(let i = 0; i < datos.length; i++){
+                todos += 'Nombre: ' + datos[i].nombre + '\n';
+                todos += 'Apellido: ' + datos[i].apellido + '\n';
+                todos += 'Facultad: ' + datos[i].facultad + '\n';
+                todos += 'Telefono: ' + datos[i].telefono + '\n';
+                todos += 'Edad: ' + datos[i].edad + '\n\n';
+            }
+
+            if(datos.length === 0){
+                alert("No hay contactos registrados");
+            } else {
+                alert(todos);
+            }
+            break;
     case 3:
         alert('Escogiste 3 salir');
-        seguir = false;
+        if (confirm('Desea salir?')){
+            seguir = false;
+        }   
         break;
     default:
         alert('No recibí una opción válida');
