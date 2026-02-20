305829d19db83309dd0b8085fcd7d2958a4ee82b
JVazquez06 <jvazquez82@ucol.mx>
2026-02-20 08:31:10 -0600

    Agenda con Objeto

--- Agenda/agenda.js
+++ Agenda/agenda.js
@@ -6,19 +6,27 @@ let option = parseInt(prompt('Escoge una opción: \n 1-Agregar \n 2-Listar \n 3-
 switch(option){
     case 1:
         alert('Escogiste la opción 1');
+        let id = datos.length+1;
         let nombre = prompt('Ingresa tu nombre');
         let apellido = prompt('Ingresa tu apellido');
         let facultad = prompt ('Ingresa la facultad a la que asistes');
-        let edad = prompt('Ingresa tu edad');
-        let telefono = ('Ingresa tu teléfono');
-        let agenda = {nombre, apellido, facultad, edad, telefono}
-        datos.push(agenda);
+        let edad = parseInt(prompt('Ingresa tu edad'));
+        let telefono = parseInt(prompt('Ingresa tu teléfono'));
+        const student = {id, nombre, apellido, facultad, edad, telefono};
+        student.id = id;
+        student.nombre = nombre;
+        student.apellido = apellido;
+        student.facultad = facultad;
+        student.edad = edad;
+        student.telefono = telefono;
+        datos.push(student);
         alert('Contacto creado');
         break;
     case 2:
         let todos = '';
 
             for(let i = 0; i < datos.length; i++){
+                todos += 'ID:' + datos[i].id + '\n';
                 todos += 'Nombre: ' + datos[i].nombre + '\n';
                 todos += 'Apellido: ' + datos[i].apellido + '\n';
                 todos += 'Facultad: ' + datos[i].facultad + '\n';
@@ -29,6 +37,7 @@ switch(option){
             if(datos.length === 0){
                 alert("No hay contactos registrados");
             } else {
+                console.log(datos);
                 alert(todos);
             }
             break;
